cebb8cbfdda94a9d8181298d429acdbf74d65152
jjesusfilho <jjesusfilho@gmail.com>
2018-02-05 15:50:10 +0000

    new update

--- R/sg_decison.R
+++ R/sg_decison.R
@@ -19,30 +19,35 @@ sg_decision <- function(string,docname,keyword,class="appeal"){
   
   df<-pt_kwic(string,docname,keyword,type="coll",before=9,after=9,unite=TRUE)
   
-  pairs1<-list(c("negaram","n\u00e3o"),c("parcial","parcial"),c("deram","sim"))
+  pairs1<-"(opin.*|pelo|requer.*|parecer|manifestou|defende.*|pleite.*|pretende)"
   
-  pairs2<-list(c("da\\-*\\s*se","sim"),
-               c("nega\\-*\\s*se","n\u00e3o"),
-               c("negar\\-*\\s*lhe","n\u00e3o"),
-               c("nao\\s*comporta","n\u00e3o"),
-               c("negram","n\u00e3o"),
-               c("negararam","n\u00e3o"),
-               c("nego","n\u00e3o"),
-               c("negar","n\u00e3o"))
+  pairs2 <- list(c("deram","sim"),
+                 c("da\\-*\\s*se", "sim"),
+                 c("dando\\-*(se)*","sim"),
+                 c("comporta.*","sim"),
+                 c("dou","sim"),
+                 c("confere\\-se","sim"),
+                 c("se\\-*da","sim"),
+                 c("merece","sim"),
+                 c("nao deram","n\u00e3o"),
+                 c("nao merece","merece"),
+                 c("se\\snega","n\u00e3o"),
+                 c("nega\\-*\\s*se", "n\u00e3o"),
+                 c("negar\\-*\\s*lhe", "n\u00e3o"), 
+                 c("nao\\s*comporta.*","n\u00e3o"),
+                 c("negram", "n\u00e3o"),
+                 c("negararam", "n\u00e3o"), 
+                 c("nego", "n\u00e3o"),
+                 c("negar", "n\u00e3o"))
   
-  df %>% 
-    dplyr::mutate(pre=stringi::stri_trans_tolower(pre),
-                  pre=stringi::stri_trans_general(pre,"latin-ascii"))
-  dplyr::filter(!stringi::stri_detect(pre,"opinou")) %>% 
-    dplyr::mutate(favor_appelant=ifs(pre,pairs1),
-                  favor_appelant=ifs(favor_appelant,pairs2)) %>% 
-    dplyr::filter(favor_appelant=="n\u00e3o",favor_appelant="sim",
-                  favor_appelant=="parcial") %>% 
-    dplyr::distinct(favor_appelant,.keep_all=TRUE) %>% 
-    dplyr::select(c("docname","favor_appelant"))
+  df %>%
+    dplyr::mutate(pre = stringi::stri_trans_tolower(pre), 
+                  pre = stringi::stri_trans_general(pre, "latin-ascii")) %>% 
+    dplyr::filter(!stringi::stri_detect_regex(pre, pairs1)) %>%
+    dplyr::mutate(favor_appelant = ifelse(str_detect(pre,"parcial"),"parcial",pre),
+                  favor_appelant = JurisMiner:::ifs(favor_appelant, pairs2)) %>% 
+    dplyr::filter(favor_appelant %in% c("n\u00e3o","sim","parcial")) %>% 
+    dplyr::distinct(docname, .keep_all = TRUE) %>%
+    dplyr::select(c("docname", "favor_appelant"))
+
 }
-
-
-
-
-
