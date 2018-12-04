d14a5916ce3d573d36a546c084797d04b7780164
JOSE DE JESUS FILHO <jjesusfilho@JOSEs-Mac-mini.local>
2018-12-04 09:18:39 -0200

    correção no cnj_sequencial

--- R/cnj_sequencial.R
+++ R/cnj_sequencial.R
@@ -17,6 +17,7 @@ cnj_sequencial <- function(inicio, fim, ano, nivel, uf, distribuidor) {
     stop("inicio e fim devem ser num\u00e9ricos")
   }
   o <- stringr::str_pad(inicio:fim, width = 7, "left", "0")
+  uf <- stringr::str_pad(uf, 2, "left", "0")
   distribuidor <- stringr::str_pad(distribuidor, 4, "left", "0")
   num <- paste0(o, ano, nivel, uf, distribuidor)
   abjutils::calc_dig(num, TRUE)
