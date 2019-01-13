7d180f601ca83c5e69f491ab3a4a1dca49b8cae8
jjesusfilho <jjesusfilho@gmail.com>
2019-01-13 13:26:59 -0200

    adiciona funcao editavel.R

---
+++ R/editavel.R
@@ -0,0 +1,39 @@
+#' Criar tabela para edição manual
+#'
+#' @param x dataframe
+#' @param coluna coluna de referência
+#' @param acao coluna indicando acao  a ser tomada
+#' @param tamanho_pagina padrão é cem, mas você pode aumentar.
+#' @param destino diretorio/arquivo onde você quer salvar o html
+#'     editável. Se não indicar, será salvo um arquivo chamdo
+#'     editavel.html no atural diretorio.
+#' @details Esta função é especialmente importante quando queremos
+#'     trabalhar em colaboração, pois ela gera um DT editável.
+#'
+#' @return arquivo html que pode ser aberto por qualqueer navegador.
+#' @export
+#'
+editavel <- function(
+
+  x=NULL,
+  coluna=NULL,
+  acao="     ",
+  tamanho_pagina=100,
+  destino="./editavel.html"
+
+) {
+
+  col <- rlang::enexpr(coluna)
+  dplyr::count(x,eval(col), sort=TRUE) %>%
+    dplyr::mutate(acao=acao) %>%
+    DT::datatable(editable=TRUE,
+                  extensions = 'Buttons',
+                  options=list(
+                    dom = 'Bfrtip',
+                    buttons = c('excel'),
+                    pageLength=tamanho_pagina)
+    ) %>%
+    DT::saveWidget(destino)
+
+
+}
