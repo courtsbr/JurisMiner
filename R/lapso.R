53c3448052efa23aefef023f9db6c278a7a4d97f
José de Jesus Filho <jjesusfilho@gmail.com>
2022-04-18 08:47:17 -0300

    ajuste do lapso

--- R/lapso.R
+++ R/lapso.R
@@ -4,7 +4,8 @@
 #' @param data_final data final
 #' @param unidade ano, mes, semana ou dia.
 #' @details Esta função é um mero wrapper das funções interval e time_length
-#'     do pacote lubridate. Ela considera as variações de ano, meses e dias.
+#'     do pacote lubridate. Ela considera as variações de 
+#'     ano, mes,semana, dia, hora, minuto e segundo.
 #'
 #' @return double com o lapso temporal
 #' @export
@@ -13,23 +14,20 @@
 #' lapso(data_inicial=as.Date("2018-01-01"),data_final=as.Date("2018-12-31"),unidade="mes")
 
 
-lapso <- function(data_inicial = NULL,
-                  data_final = NULL,
-                  unidade = "mes") {
-  if (!lubridate::is.Date(data_inicial) |
-      !lubridate::is.Date(data_final)) {
+lapso <- function (data_inicial = NULL, data_final = NULL, unidade = "mes")
+{
+  if (!lubridate::is.timepoint(data_inicial) | !lubridate::is.timepoint(data_final)) {
     stop("As datas devem no formato data")
   }
+  periodo <- switch(unidade, 
+                    ano = "year", 
+                    mes = "month", 
+                    semana = "week",
+                    dia = "day",
+                    hora = "hour",
+                    minuto = "minute",
+                    segundo = "second")
   
-  periodo <- switch(
-    unidade,
-    "ano" = "year",
-    "mes" = "month",
-    "semana" = "week",
-    "dia" = "day"
-  )
-  
-  lubridate::interval(data_inicial, data_final) %>%
+  lubridate::interval(data_inicial, data_final) |> 
     lubridate::time_length(periodo)
-  
 }
