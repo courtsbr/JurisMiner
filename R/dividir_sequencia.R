41da81d68ba5acc168f9195fe3250ad638745e50
José <jjesusfilho@gmail.com>
2023-04-03 09:25:33 -0300

    melhora dividir_sequencia

--- R/dividir_sequencia.R
+++ R/dividir_sequencia.R
@@ -1,16 +1,37 @@
 #' Divide sequência em n grupos 
 #'
 #' @param sequencia Sequência
-#' @param n Número de grupos
-#'
+#' @param g Número de grupos.
+#' @param n Alternativamente número de elementos por grupo.
 #' @return Lista
 #' @export
 #'
 #' @examples
 #' dividir_sequencia(1:100, 5)
-dividir_sequencia <- function(sequencia, n){
+dividir_sequencia <- function(sequencia, g = NULL, n = NULL){
   
- split(sequencia, cut(seq_along(sequencia),n,labels =F))
+  if (!is.null(g) & !is.null(n)){
+    
+    stop("Você deve informar g ou n, nunca os dois.")
+  }
+  
+  if (is.null(g) & is.null(n)){
+    
+    stop("Você tem de informar g ou n")
+  }
+  
+ if (!is.null(g)){
+   
+  grupos <-  split(sequencia, cut(seq_along(sequencia),g,labels =F))
+   
+  return(grupos)
+ }
 
+ if (!is.null(n)){
+   
+   numero <- split(sequencia, ceiling(seq_along(sequencia)/n))
+   
+   return(numero)
+ }
   
 }
